ce47e5ba2dcd8f702c82222baad6d63825aec04d
Antonin Vychodil <avychodil@monetplus.cz>
2025-02-18 10:52:32 +0100

    React Routing

--- 02-backend/spring-boot-library/src/main/java/com/tvtoner/spring_boot_library/dao/BookRepository.java
+++ 02-backend/spring-boot-library/src/main/java/com/tvtoner/spring_boot_library/dao/BookRepository.java
@@ -1,11 +1,18 @@
 package com.tvtoner.spring_boot_library.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.tvtoner.spring_boot_library.entity.Book;
 
 @RepositoryRestResource(excerptProjection = FullBookProjection.class)
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
+
+    Page<Book> findByCategory(@RequestParam("category") String category, Pageable pageable);
 
 }
